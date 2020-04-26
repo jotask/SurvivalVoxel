5eb2d76431452e6eafa87929153cab9ad152452b
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-26 13:43:04 +0200

    basic engine

---
+++ src/engine.cpp
@@ -0,0 +1,78 @@
+#include "engine.hpp"
+
+#include "system/display_system.hpp"
+#include "system/render_system.hpp"
+#include "system/imgui_system.hpp"
+#include "system/system_connector.hpp"
+
+#include <algorithm>
+
+// TODO : Remove this dependency and use some event system to remove dependency between system
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace engine
+{
+    Engine::Engine()
+        : m_displaySystem(nullptr)
+        , m_renderSystem(nullptr)
+    {
+
+    }
+
+    void Engine::run()
+    {
+
+        init();
+
+        auto* window = m_displaySystem->getWindow();
+
+        while (glfwWindowShouldClose(window) == false)
+        {
+            
+            // Poll window events
+            // TODO : Replace this with a input module
+            glfwPollEvents();
+
+            // Update all systems
+            std::for_each(m_systems.begin(), m_systems.end(), [](auto& sys) { sys->preUpdate(); });
+            std::for_each(m_systems.begin(), m_systems.end(), [](auto& sys) { sys->update(); });
+            std::for_each(m_systems.begin(), m_systems.end(), [](auto& sys) { sys->postUpdate(); });
+
+            // TODO : This can be moved to their proper class
+            int display_w = 0;
+            int display_h = 0;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClear(GL_COLOR_BUFFER_BIT);
+        
+            // Render all systems
+            std::for_each(m_systems.begin(), m_systems.end(), [](auto& sys) { sys->preRender(); });
+            std::for_each(m_systems.begin(), m_systems.end(), [](auto& sys) { sys->render(); });
+            std::for_each(m_systems.begin(), m_systems.end(), [](auto& sys) { sys->postRender(); });
+
+            glfwSwapBuffers(window);
+        }
+
+    }
+
+    void Engine::init()
+    {
+
+        // Create all systems first
+        m_systems.push_back(std::make_unique<DisplaySystem>());
+        m_systems.push_back(std::make_unique<RenderSystem>());
+        m_systems.push_back(std::make_unique<ImguiSystem>());
+
+        // Connect all system before we initialize them
+        auto systemConnector = SystemConnector(m_systems);
+        m_displaySystem = systemConnector.findSystem<DisplaySystem>();
+        m_renderSystem = systemConnector.findSystem<RenderSystem>();
+        std::for_each(m_systems.begin(), m_systems.end(), [&systemConnector](auto& sys) { sys->connect(systemConnector); });
+
+        // Initialise all systems
+        std::for_each(m_systems.begin(), m_systems.end(), [](auto& sys) { sys->init(); });
+
+    }
+
+}
---
+++ src/engine.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include <vector>
+
+#include "shared.hpp"
+#include "system/system_interface.hpp"
+
+namespace engine
+{
+
+    class DisplaySystem;
+    class RenderSystem;
+
+    class Engine
+    {
+    public:
+        Engine();
+        ~Engine() = default;
+
+        void run();
+
+    private:
+
+        void init();
+
+        std::vector<AikoUPtr<System>> m_systems;
+
+        DisplaySystem*  m_displaySystem;
+        RenderSystem*   m_renderSystem;
+
+    };
+
+}
+
