4e7e71dbfae3339a0beeafcd1960ee88ccdd9439
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-26 18:09:01 +0200

    Fix indentation

--- src/system/display_system.hpp
+++ src/system/display_system.hpp
@@ -11,25 +11,25 @@ namespace engine
 
     class DisplaySystem : public System
     {
-        public:
+    public:
 
-            DisplaySystem();
-            virtual ~DisplaySystem() override;
+        DisplaySystem();
+        virtual ~DisplaySystem() override;
 
-            virtual bool init() override;
-            virtual bool connect(SystemConnector&) override;
+        virtual bool init() override;
+        virtual bool connect(SystemConnector&) override;
 
-            virtual void preUpdate() override;
-            virtual void update() override;
-            virtual void postUpdate() override;
+        virtual void preUpdate() override;
+        virtual void update() override;
+        virtual void postUpdate() override;
 
-            virtual void preRender() override;
-            virtual void render() override;
-            virtual void postRender() override;
+        virtual void preRender() override;
+        virtual void render() override;
+        virtual void postRender() override;
 
-            GLFWwindow* getWindow() const;
+        GLFWwindow* getWindow() const;
 
-            glm::vec2 getWindowSize() const;
+        glm::vec2 getWindowSize() const;
 
     private:
 
