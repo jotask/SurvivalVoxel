0ca260791aa427218bbec71338b41522c2ef4bb8
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-26 13:41:10 +0200

    System connector to connect all systems

---
+++ src/system/system_connector.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vector>
+
+#include "shared.hpp"
+
+namespace engine
+{
+
+    class System;
+
+    class SystemConnector
+    {
+    public:
+
+        using Systems = std::vector<AikoUPtr<System>>;
+
+        SystemConnector(Systems& systems)
+            : m_systems(systems)
+        {
+
+        }
+
+        template <class T>
+        T* findSystem()
+        {
+            for (auto& m : m_systems)
+            {
+                if (T* tmp = dynamic_cast<T*>(m.get())) {
+                    return tmp;
+                }
+            }
+            return nullptr;
+        }
+
+    private:
+
+        Systems& m_systems;
+
+    };
+
+}
