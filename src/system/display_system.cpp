2eb2b1f1d6a4f60c14937e39a1c7d0d327ad43a6
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-26 13:41:35 +0200

    Display system to handle window

---
+++ src/system/display_system.cpp
@@ -0,0 +1,97 @@
+#include "system/display_system.hpp"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace engine
+{
+    DisplaySystem::DisplaySystem()
+        : m_window(nullptr)
+    {
+
+    }
+
+    DisplaySystem::~DisplaySystem()
+    {
+        glfwTerminate();
+    }
+
+    bool DisplaySystem::init()
+    {
+        if (glfwInit() == false)
+        {
+            fprintf(stderr, "Failed to init GLFW\n");
+            return false;
+        }
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        printf("GLFW version: %s\n", glfwGetVersionString());
+
+        m_window = glfwCreateWindow(500, 300, "Window Title", NULL, NULL);
+
+        if (m_window == nullptr)
+        {
+            fprintf(stderr, "Failed to create GLFW window");
+            return false;
+        }
+
+        glfwMakeContextCurrent(m_window);
+
+        if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == false)
+        {
+            fprintf(stderr, "Failed to init GLAD\n");
+            return false;
+        }
+
+        printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+
+        glfwSwapInterval(1);
+
+        return true;
+
+    }
+
+    bool DisplaySystem::connect(SystemConnector &)
+    {
+        return true;
+    }
+
+    void DisplaySystem::preUpdate()
+    {
+
+    }
+
+    void DisplaySystem::update()
+    {
+
+    }
+
+    void DisplaySystem::postUpdate()
+    {
+
+    }
+
+    void DisplaySystem::preRender()
+    {
+
+    }
+
+    void DisplaySystem::render()
+    {
+
+    }
+
+    void DisplaySystem::postRender()
+    {
+
+    }
+
+    GLFWwindow * DisplaySystem::getWindow() const
+    {
+        return m_window;
+    }
+
+}
---
+++ src/system/display_system.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "system/system_interface.hpp"
+
+struct GLFWwindow;
+
+namespace engine
+{
+
+    class DisplaySystem : public System
+    {
+        public:
+
+            DisplaySystem();
+            virtual ~DisplaySystem() override;
+
+            virtual bool init() override;
+            virtual bool connect(SystemConnector&) override;
+
+            virtual void preUpdate() override;
+            virtual void update() override;
+            virtual void postUpdate() override;
+
+            virtual void preRender() override;
+            virtual void render() override;
+            virtual void postRender() override;
+
+            GLFWwindow* getWindow() const;
+
+    private:
+
+        GLFWwindow* m_window;
+
+    };
+
+}
