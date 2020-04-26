b7d8f8c4df0c1b7cc6dadf85432c18e940f0b6e0
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-26 12:24:21 +0200

    Adding basic project files

---
+++ src/engine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <imgui.h>
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl3.h>
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
