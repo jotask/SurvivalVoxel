5418677aa5ac8f2a274417dea22355eb8a4755d5
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-26 13:40:52 +0200

    Basic system interface

---
+++ src/system/system_interface.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "system/system_connector.hpp"
+
+namespace engine
+{
+    class System
+    {
+        public:
+            System() = default;
+            virtual ~System() = default;
+
+            virtual bool connect(SystemConnector&) { return true; };
+
+            virtual bool init() { return true; };
+
+            virtual void preUpdate() { };
+            virtual void update() { };
+            virtual void postUpdate() { };
+
+            virtual void preRender() { };
+            virtual void render() { };
+            virtual void postRender() { };
+
+    };
+}
