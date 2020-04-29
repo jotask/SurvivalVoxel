862b977b15f0182c8061311fdfcdce9582a87088
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-29 11:13:43 +0200

    Adding commented coded for render with wireframe

--- src/system/render_system.cpp
+++ src/system/render_system.cpp
@@ -44,6 +44,7 @@ namespace engine
     {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     }
 
     void RenderSystem::render()
