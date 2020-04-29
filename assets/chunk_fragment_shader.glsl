6bb4956001e573e46656b5b64b8add23b2933be3
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-29 17:45:24 +0200

    Update chunk shader to include lighting

--- assets/chunk_fragment_shader.glsl
+++ assets/chunk_fragment_shader.glsl
@@ -2,8 +2,23 @@
 layout(location = 0) out vec4 out_color;
 
 in vec4 color;
+in vec3 normal;
+in vec3 fragPos;
+in vec3 lightPos;
+in vec3 lightCol;
 
 void main()
 {
-  out_color = color;
+
+  vec3 norm = normalize(normal);
+  vec3 lightDir = normalize(lightPos - fragPos);
+
+  float diff = max(dot(norm, lightDir), 0.0f);
+  vec3 diffuse = diff * lightCol;
+
+  // vec3 result = (ambient + diffuse) * color.xyz;
+  vec3 result = (diffuse) * color.xyz;
+
+  out_color = vec4(result, 1.0f);
+
 }
--- assets/chunk_vertex_shader.glsl
+++ assets/chunk_vertex_shader.glsl
@@ -1,18 +1,29 @@
 #version 450 core
 
 layout(location = 0) in vec3 in_position;
-layout(location = 1) in vec4 in_color;
+layout(location = 1) in vec3 in_normal;
+layout(location = 2) in vec4 in_color;
 
 uniform mat4 projection_matrix;
 uniform mat4 view_matrix;
 uniform mat4 model_matrix;
+uniform vec3 u_lightPos;
+uniform vec3 u_lightCol;
 
 out vec4 color;
+out vec3 fragPos;
+out vec3 normal;
+out vec3 lightPos;
+out vec3 lightCol;
 
 void main()
 {
 
   color = in_color;
+  normal = in_normal;
+  lightPos = u_lightPos;
+  lightCol = u_lightCol;
+  fragPos = vec3(model_matrix * vec4(in_position, 1.0f));
 
   gl_Position = projection_matrix * view_matrix * model_matrix * vec4(in_position, 1);
 
