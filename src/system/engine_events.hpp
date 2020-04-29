d0771077739f593b377fc9b9234d29e562df707d
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-29 17:37:54 +0200

    Add basic engine events

---
+++ src/system/engine_events.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "system/event.hpp"
+
+namespace engine
+{
+
+    class WindowCloseEvent : public Event
+    {
+    public:
+        WindowCloseEvent() = default;
+        virtual ~WindowCloseEvent() = default;
+        virtual EventId     getId() const { return "WindowCloseEvent"; }
+    };
+
+    class WindowResizeEvent : public Event
+    {
+    public:
+        WindowResizeEvent()
+            : width(-1)
+            , height(-1)
+        {
+
+        };
+
+        WindowResizeEvent(const int w, const int h)
+            : width(w)
+            , height(h)
+        {
+
+        };
+
+        virtual ~WindowResizeEvent() = default;
+        virtual EventId     getId() const { return "WindowResizeEvent"; }
+
+        const int width;
+        const int height;
+    };
+
+    class OnMouseEnterEvent : public Event
+    {
+    public:
+        OnMouseEnterEvent() : entered(false) {};
+        OnMouseEnterEvent(const bool enter) : entered(enter) {};
+        virtual ~OnMouseEnterEvent() = default;
+        virtual EventId     getId() const { return "OnMouseEnterEvent"; }
+        const bool entered;
+    };
+
+    class OnKeyPressedEvent : public Event
+    {
+    public:
+        OnKeyPressedEvent() = default;
+        OnKeyPressedEvent(int key, int scancode, int action, int mods) : key(key), scancode(scancode), action(action), mods(mods) {  };
+        virtual ~OnKeyPressedEvent() = default;
+        virtual EventId     getId() const { return "OnKeyPressedEvent"; }
+        int key; int scancode; int action; int mods;
+    };
+
+    class OnMouseMoveEvent : public Event
+    {
+    public:
+        OnMouseMoveEvent() = default;
+        OnMouseMoveEvent(float x, float y, float prevX, float prevY) : x(x), y(y), prevX(prevX), prevY(prevY)
+        {
+            dx = x - prevX;
+            dy = prevY - y;
+        };
+        virtual ~OnMouseMoveEvent() = default;
+        virtual EventId     getId() const { return "OnMouseMoveEvent"; }
+        float x; float y;
+        float prevX; float prevY;
+        float dx, dy;
+    };
+
+}
