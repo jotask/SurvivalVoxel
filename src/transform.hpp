4f6417aae7ad9eeed2f21ceb363365f3506a3e6c
Jose Vives Iznardo <acachitoro@gmail.com>
2020-04-29 11:13:14 +0200

    Implementing basic transform class

---
+++ src/transform.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace engine
+{
+
+    class Transform
+    {
+    public:
+
+        glm::vec3 position;
+        glm::vec3 rotation;
+        glm::vec3 scale;
+
+        Transform()
+            : position(0.f, 0.f, 0.f)
+            , rotation(0.f, 0.f, 0.f)
+            , scale(1.f, 1.f, 1.f)
+        {
+
+        }
+
+        Transform(glm::vec3 position)
+            : position(position)
+            , rotation(0.f, 0.f, 0.f)
+            , scale (1.f, 1.f, 1.f)
+        {
+
+        }
+
+        auto getModelMatrix() const
+        {
+            auto model = glm::mat4(1.f);
+            model = glm::translate(model, position);
+            return model;
+        }
+
+    };
+
+}
