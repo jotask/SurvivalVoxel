600873156059925daf5506d8858981d768b7db36
Jose Vives Iznardo <acachitoro@gmail.com>
2020-05-03 13:55:56 +0200

    Upgrade shaders

--- assets/chunk_fragment_shader.glsl
+++ assets/chunk_fragment_shader.glsl
@@ -1,24 +1,50 @@
 #version 450 core
-layout(location = 0) out vec4 out_color;
+out vec4 FragColor;
 
-in vec4 color;
-in vec3 normal;
-in vec3 fragPos;
-in vec3 lightPos;
-in vec3 lightCol;
+struct Material
+{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+
+struct Light
+{
+    vec3 position;
+    vec3 color;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec4 Color;
+
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
 
 void main()
 {
 
-  vec3 norm = normalize(normal);
-  vec3 lightDir = normalize(lightPos - fragPos);
+    // ambient
+    vec3 ambient = light.ambient * material.ambient;
 
-  float diff = max(dot(norm, lightDir), 0.0f);
-  vec3 diffuse = diff * lightCol;
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
-  // vec3 result = (ambient + diffuse) * color.xyz;
-  vec3 result = (diffuse) * color.xyz;
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);  
 
-  out_color = vec4(result, 1.0f);
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
 
 }
--- assets/chunk_vertex_shader.glsl
+++ assets/chunk_vertex_shader.glsl
@@ -1,30 +1,20 @@
 #version 450 core
+layout (location = 0) in vec3 inPosition;
+layout (location = 1) in vec3 inNormal;
+layout (location = 2) in vec4 inColor;
 
-layout(location = 0) in vec3 in_position;
-layout(location = 1) in vec3 in_normal;
-layout(location = 2) in vec4 in_color;
+out vec3 FragPos;
+out vec3 Normal;
+out vec4 Color;
 
-uniform mat4 projection_matrix;
-uniform mat4 view_matrix;
-uniform mat4 model_matrix;
-uniform vec3 u_lightPos;
-uniform vec3 u_lightCol;
-
-out vec4 color;
-out vec3 fragPos;
-out vec3 normal;
-out vec3 lightPos;
-out vec3 lightCol;
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
 
 void main()
 {
-
-  color = in_color;
-  normal = in_normal;
-  lightPos = u_lightPos;
-  lightCol = u_lightCol;
-  fragPos = vec3(model_matrix * vec4(in_position, 1.0f));
-
-  gl_Position = projection_matrix * view_matrix * model_matrix * vec4(in_position, 1);
-
+  Color = inColor;
+  FragPos = vec3(model * vec4(inPosition, 1.0));
+  Normal = mat3(transpose(inverse(model))) * inNormal;
+  gl_Position = projection * view * vec4(FragPos, 1.0);
 }
