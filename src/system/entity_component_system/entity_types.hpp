b31fe24daa37601a466680a4704d5de24f4905d9
Jose Vives Iznardo <acachitoro@gmail.com>
2020-05-07 11:05:49 +0200

    Created entity type

---
+++ src/system/entity_component_system/entity_types.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "utils/shared.hpp"
+
+#include <cstddef>
+#include <vector>
+
+namespace engine
+{
+
+    class Entity;
+
+    namespace entity
+    {
+        using EntityId = std::size_t;
+        using ComponentId = std::size_t;
+        using EntityCollection = std::vector<AikoUPtr<Entity>>;
+
+        enum class EntityTag
+        {
+            DEFAULT,
+            LIGHT,
+            CHUNK
+        };
+
+    }
+}
