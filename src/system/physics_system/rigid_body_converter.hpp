5971ddd94905bbf6fd2f550eaa8c83b5f2f07344
Jose Vives Iznardo <acachitoro@gmail.com>
2020-05-10 15:29:39 +0200

    Making class as static

--- src/system/physics_system/rigid_body_converter.hpp
+++ src/system/physics_system/rigid_body_converter.hpp
@@ -11,25 +11,25 @@ namespace engine
     {
         namespace converter
         {
-            glm::vec3 bulletToGlm(const btVector3& v) { return glm::vec3(v.getX(), v.getY(), v.getZ()); }
+            static glm::vec3 bulletToGlm(const btVector3& v) { return glm::vec3(v.getX(), v.getY(), v.getZ()); }
 
-            btVector3 glmToBullet(const glm::vec3& v) { return btVector3(v.x, v.y, v.z); }
+            static btVector3 glmToBullet(const glm::vec3& v) { return btVector3(v.x, v.y, v.z); }
 
-            glm::quat bulletToGlm(const btQuaternion& q) { return glm::quat(q.getW(), q.getX(), q.getY(), q.getZ()); }
+            static glm::quat bulletToGlm(const btQuaternion& q) { return glm::quat(q.getW(), q.getX(), q.getY(), q.getZ()); }
 
-            btQuaternion glmToBullet(const glm::quat& q) { return btQuaternion(q.x, q.y, q.z, q.w); }
+            static btQuaternion glmToBullet(const glm::quat& q) { return btQuaternion(q.x, q.y, q.z, q.w); }
 
-            btMatrix3x3 glmToBullet(const glm::mat3& m) { return btMatrix3x3(m[0][0], m[1][0], m[2][0], m[0][1], m[1][1], m[2][1], m[0][2], m[1][2], m[2][2]); }
+            static btMatrix3x3 glmToBullet(const glm::mat3& m) { return btMatrix3x3(m[0][0], m[1][0], m[2][0], m[0][1], m[1][1], m[2][1], m[0][2], m[1][2], m[2][2]); }
 
             // btTransform does not contain a full 4x4 matrix, so this transform is lossy.
             // Affine transformations are OK but perspective transformations are not.
-            btTransform glmToBullet(const glm::mat4& m)
+            static btTransform glmToBullet(const glm::mat4& m)
             {
                 glm::mat3 m3(m);
                 return btTransform(glmToBullet(m3), glmToBullet(glm::vec3(m[3][0], m[3][1], m[3][2])));
             }
 
-            glm::mat4 bulletToGlm(const btTransform& t)
+            static glm::mat4 bulletToGlm(const btTransform& t)
             {
                 glm::mat4 m(0.f);
                 const btMatrix3x3& basis = t.getBasis();
