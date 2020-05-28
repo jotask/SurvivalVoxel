93740d97774c7822c84149105e9b979dee65605b
Jose Vives Iznardo <acachitoro@gmail.com>
2020-05-28 19:36:58 +0200

    Adding opengl debug output for errors, this make life so easier

--- src/systems/display_system.cpp
+++ src/systems/display_system.cpp
@@ -30,6 +30,7 @@ namespace aiko
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
 
         printf("GLFW version: %s\n", glfwGetVersionString());
 
@@ -55,6 +56,58 @@ namespace aiko
 
         glfwSetWindowSizeCallback(m_window,  [](GLFWwindow* window, int width, int height) { EventSystem::it().sendEvent(WindowResizeEvent(width, height)); });
         glfwSetWindowCloseCallback(m_window, [](GLFWwindow* window) { EventSystem::it().sendEvent(WindowCloseEvent()); });
+
+        int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+        if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+        {
+
+            auto glDebugOutput = [](GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char *message, const void *userParam)
+            {
+                // ignore non-significant error/warning codes
+                if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+                std::cout << "---------------" << std::endl;
+                std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+                switch (source)
+                {
+                case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+                case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+                case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+                case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+                case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+                case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+                } std::cout << std::endl;
+
+                switch (type)
+                {
+                case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+                case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+                case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+                case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+                case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+                case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+                case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+                case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+                case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+                } std::cout << std::endl;
+
+                switch (severity)
+                {
+                case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+                case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+                case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+                case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+                } std::cout << std::endl;
+                std::cout << std::endl;
+            };
+
+            glEnable(GL_DEBUG_OUTPUT);
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+            glDebugMessageCallback(glDebugOutput, nullptr);
+            glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
+        }
 
         return true;
 
