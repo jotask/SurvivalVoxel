8db3efadb70404c9c386f17fb49e88933a14e023
Jose Vives Iznardo <acachitoro@gmail.com>
2020-06-03 19:35:46 +0200

    implement light calculations for texture shader

--- assets/shaders/static_texture_fragment_shader.glsl
+++ assets/shaders/static_texture_fragment_shader.glsl
@@ -1,23 +1,54 @@
 #version 450 core
 
+const int MAX_LIGHT_NUMBER = 4;
+
 struct Light
 {
     vec3 position;
     vec3 color;
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
 };
 
-in vec4 Colour;
-in vec2 TexCoords;
+in vec2 pass_textureCoordinates;
+in vec3 surfaceNormal;
+in vec3 toLightVector[MAX_LIGHT_NUMBER];
+in vec3 toCameraVector;
 
 out vec4 out_color;
 
 uniform sampler2D texture_diffuse_1;
-uniform Light light;
+uniform Light light[MAX_LIGHT_NUMBER];
+
+uniform float shineDamper;
+uniform float reflectivity;
 
 void main()
 {
-    out_color = texture(texture_diffuse_1, TexCoords);
-}
+
+    vec3 unitNormal = normalize(surfaceNormal);
+    vec3 unitVectorToCamera = normalize(toCameraVector);
+
+    vec3 totalDiffuse = vec3(0.0f);
+    vec3 totalSpecular = vec3(0.0f);
+
+    for(int i = 0 ; i < MAX_LIGHT_NUMBER ; i++)
+    {
+        vec3 unitLightVector = normalize(toLightVector[i]);
+        float nDot1 = dot(unitNormal, unitLightVector);
+        float brightness = max(nDot1, 0.0f);
+        vec3 lightDirection = -unitLightVector;
+        vec3 reflectedLightDirection = reflect(lightDirection, unitNormal);
+        float specularFactor = max(dot(reflectedLightDirection, unitVectorToCamera), 0.0f);
+        float dampedFactor = pow(specularFactor, shineDamper);
+        totalDiffuse = totalDiffuse + brightness * light[i].color;
+        totalSpecular = totalSpecular + dampedFactor * reflectivity * light[i].color;
+    }
+
+    totalDiffuse = max(totalDiffuse, 0.2);
+
+    vec4 textureColor = texture(texture_diffuse_1, pass_textureCoordinates);
+    if(textureColor.a < 0.5)
+    {
+        discard;
+    }
+
+    out_color = vec4(totalDiffuse, 1.0f) * textureColor + vec4(totalSpecular, 1.0f);}
--- assets/shaders/static_texture_vertex_shader.glsl
+++ assets/shaders/static_texture_vertex_shader.glsl
@@ -1,12 +1,11 @@
 #version 450 core
 
+const int MAX_LIGHT_NUMBER = 4;
+
 struct Light
 {
     vec3 position;
     vec3 color;
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
 };
 
 layout (location = 0) in vec3 position;
@@ -17,14 +16,22 @@ layout (location = 3) in vec2 textureCoordinates;
 uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
-uniform Light light;
+uniform Light light[MAX_LIGHT_NUMBER];
 
-out vec4 Colour;
-out vec2 TexCoords;
+out vec2 pass_textureCoordinates;
+out vec3 surfaceNormal;
+out vec3 toLightVector[MAX_LIGHT_NUMBER];
+out vec3 toCameraVector;
 
 void main()
 {
-  Colour = color;
-  TexCoords = textureCoordinates;
-  gl_Position = projection * view * model * vec4(position, 1.0);
+  vec4 worldPosition = model * vec4(position, 1.f);
+  gl_Position = projection * view * worldPosition;
+  pass_textureCoordinates = textureCoordinates;
+  surfaceNormal = (model * vec4(normal, 0.0f)).xyz;
+  for(int i = 0 ; i < MAX_LIGHT_NUMBER; i++)
+  {
+    toLightVector[i] = light[i].position - worldPosition.xyz;
+  }
+  toCameraVector = (inverse(view) * vec4(0.0, 0.0f, 0.0, 1.0f)).xyz - worldPosition.xyz;
 }
