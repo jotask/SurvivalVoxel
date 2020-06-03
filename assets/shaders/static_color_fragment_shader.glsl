1304c621b493190dbebbd0aa641dda6a1d310f6b
Jose Vives Iznardo <acachitoro@gmail.com>
2020-06-03 19:21:28 +0200

    Adding multiple light support

--- assets/shaders/static_color_fragment_shader.glsl
+++ assets/shaders/static_color_fragment_shader.glsl
@@ -1,22 +1,21 @@
 #version 450 core
 
+const int MAX_LIGHT_NUMBER = 4;
+
 struct Light
 {
     vec3 position;
     vec3 color;
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
 };
 
 in vec2 pass_textureCoordinates;
 in vec3 surfaceNormal;
-in vec3 toLightVector;
+in vec3 toLightVector[MAX_LIGHT_NUMBER];
 in vec3 toCameraVector;
 
 out vec4 out_color;
 
-uniform Light light;
+uniform Light light[MAX_LIGHT_NUMBER];
 
 uniform float shineDamper;
 uniform float reflectivity;
@@ -25,20 +24,26 @@ void main()
 {
 
     vec3 unitNormal = normalize(surfaceNormal);
-    vec3 unitLightVector = normalize(toLightVector);
-
-    float nDot1 = dot(unitNormal, unitLightVector);
-    float brightness = max(nDot1, 0.0f);
-    vec3 diffuse = brightness * light.color;
-
     vec3 unitVectorToCamera = normalize(toCameraVector);
-    vec3 lightDirection = -unitLightVector;
-    vec3 reflectedLightDirection = reflect(lightDirection, unitNormal);
 
-    float specularFactor = max(dot(reflectedLightDirection, unitVectorToCamera), 0.0f);
-    float dampedFactor = pow(specularFactor, shineDamper);
-    vec3 finalSpecular = dampedFactor * reflectivity * light.color;
+    vec3 totalDiffuse = vec3(0.0f);
+    vec3 totalSpecular = vec3(0.0f);
 
-    out_color = vec4(diffuse, 1.0f) /* * texture(modelTexture, pass_textureCoordinates) */ + vec4(finalSpecular, 1.0f);
+    for(int i = 0 ; i < MAX_LIGHT_NUMBER ; i++)
+    {
+        vec3 unitLightVector = normalize(toLightVector[i]);
+        float nDot1 = dot(unitNormal, unitLightVector);
+        float brightness = max(nDot1, 0.0f);
+        vec3 lightDirection = -unitLightVector;
+        vec3 reflectedLightDirection = reflect(lightDirection, unitNormal);
+        float specularFactor = max(dot(reflectedLightDirection, unitVectorToCamera), 0.0f);
+        float dampedFactor = pow(specularFactor, shineDamper);
+        totalDiffuse = totalDiffuse + brightness * light[i].color;
+        totalSpecular = totalSpecular + dampedFactor * reflectivity * light[i].color;
+    }
+
+    totalDiffuse = max(totalDiffuse, 0.2);
+
+    out_color = vec4(totalDiffuse, 1.0f) + vec4(totalSpecular, 1.0f);
 
 }
--- assets/shaders/static_color_vertex_shader.glsl
+++ assets/shaders/static_color_vertex_shader.glsl
@@ -1,12 +1,11 @@
 #version 450 core
 
+const int MAX_LIGHT_NUMBER = 4;
+
 struct Light
 {
     vec3 position;
     vec3 color;
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
 };
 
 layout (location = 0) in vec3 position;
@@ -17,11 +16,11 @@ layout (location = 3) in vec2 textureCoordinates;
 uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
-uniform Light light;
+uniform Light light[MAX_LIGHT_NUMBER];
 
 out vec2 pass_textureCoordinates;
 out vec3 surfaceNormal;
-out vec3 toLightVector;
+out vec3 toLightVector[MAX_LIGHT_NUMBER];
 out vec3 toCameraVector;
 
 void main()
@@ -30,6 +29,9 @@ void main()
   gl_Position = projection * view * worldPosition;
   pass_textureCoordinates = textureCoordinates;
   surfaceNormal = (model * vec4(normal, 0.0f)).xyz;
-  toLightVector = light.position - worldPosition.xyz;
+  for(int i = 0 ; i < MAX_LIGHT_NUMBER; i++)
+  {
+    toLightVector[i] = light[i].position - worldPosition.xyz;
+  }
   toCameraVector = (inverse(view) * vec4(0.0, 0.0f, 0.0, 1.0f)).xyz - worldPosition.xyz;
 }
