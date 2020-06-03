e628b1f0a8439ec5c509b35200b1f59da512e284
Jose Vives Iznardo <acachitoro@gmail.com>
2020-06-03 15:02:32 +0200

    Move shaders to specific folder

--- src/systems/shader_system/shader_system.cpp
+++ src/systems/shader_system/shader_system.cpp
@@ -18,13 +18,13 @@ namespace aiko
 
     bool ShaderSystem::init()
     {
-        createProgram("staticShader", "static_vertex_shader.glsl", "static_fragment_shader.glsl");
-        createProgram("staticColorShader", "static_color_vertex_shader.glsl", "static_fragment_shader.glsl");
-        createProgram("staticTextureShader", "static_texture_vertex_shader.glsl", "static_fragment_shader.glsl");
-        createProgram("colorShader", "vertex_shader.glsl", "fragment_shader.glsl");
-        createProgram("cubeShader", "cube_vertex_shader.glsl", "cube_fragment_shader.glsl");
-        createProgram("chunkShader", "chunk_vertex_shader.glsl", "chunk_fragment_shader.glsl");
-        createProgram("bullet3Shader", "bullet3_vertex_shader.glsl", "bullet3_fragment_shader.glsl");
+        createProgram("staticShader", "shaders/static_vertex_shader.glsl", "shaders/static_fragment_shader.glsl");
+        createProgram("staticColorShader", "shaders/static_color_vertex_shader.glsl", "shaders/static_fragment_shader.glsl");
+        createProgram("staticTextureShader", "shaders/static_texture_vertex_shader.glsl", "shaders/static_fragment_shader.glsl");
+        createProgram("colorShader", "shaders/vertex_shader.glsl", "shaders/fragment_shader.glsl");
+        createProgram("cubeShader", "shaders/cube_vertex_shader.glsl", "shaders/cube_fragment_shader.glsl");
+        createProgram("chunkShader", "shaders/chunk_vertex_shader.glsl", "shaders/chunk_fragment_shader.glsl");
+        createProgram("bullet3Shader", "shaders/bullet3_vertex_shader.glsl", "shaders/bullet3_fragment_shader.glsl");
         return true;
     }
 
