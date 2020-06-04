444c877ddfa2979b56d39e7663c68a689632c535
Jose Vives Iznardo <acachitoro@gmail.com>
2020-06-04 09:15:06 +0200

    Improve light system

--- assets/shaders/light_fragment_shader.glsl
+++ assets/shaders/light_fragment_shader.glsl
@@ -7,5 +7,5 @@ in vec3 colour;
 void main()
 {
   out_color = vec4(colour, 1.0f);
-  out_color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+  //out_color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
--- assets/shaders/light_vertex_shader.glsl
+++ assets/shaders/light_vertex_shader.glsl
@@ -1,13 +1,10 @@
 #version 450 core
 
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 normal;
-layout (location = 2) in vec4 color;
-layout (location = 3) in vec2 textureCoordinates;
 
 uniform mat4 projection;
 uniform mat4 view;
-uniform vec3 lightPos;
+uniform mat4 model;
 uniform vec3 lightColor;
 
 out vec3 colour;
@@ -15,5 +12,5 @@ out vec3 colour;
 void main()
 {
   colour = lightColor;
-  gl_Position = projection * view * vec4(position, 1.0f);
+  gl_Position = projection * view * model * vec4(position, 1.0f);
 }
