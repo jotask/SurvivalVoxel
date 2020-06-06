5aca0a43f1d1f751e09e8b9a0dc3a9b00718bb6f
Jose Vives Iznardo <acachitoro@gmail.com>
2020-06-06 21:04:13 +0200

    Include rotation into model matrix calculations

--- src/utils/transform.hpp
+++ src/utils/transform.hpp
@@ -34,6 +34,9 @@ namespace aiko
         {
             auto model = glm::mat4(1.f);
             model = glm::translate(model, position);
+            model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+            model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
             model = glm::scale(model, scale);
             return model;
         }
