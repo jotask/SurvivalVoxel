6292728868b041d97eca22f54bb09f984b87ebbf
Jose Vives Iznardo <acachitoro@gmail.com>
2020-06-07 20:43:13 +0200

    Using new mesh code for meshes

--- src/systems/entity_component_system/components/mesh_component.cpp
+++ src/systems/entity_component_system/components/mesh_component.cpp
@@ -1,140 +1,33 @@
 #include "mesh_component.hpp"
 
 #include "systems/entity_component_system/entity.hpp"
-#include "systems/render_system/vertex_info.hpp"
-#include "engine.hpp"
 
-#include <imgui.h>
-
-#include <iostream>
+#include "systems/assets_system/mesh.hpp"
+#include "systems/shader_system/shader.hpp"
 
 namespace aiko
 {
 
-    Mesho::Mesho(Entity* entity, Shader& shader, MeshData& meshData)
+    MeshComponent::MeshComponent(Entity* entity, Mesh* mesh, Shader* shader)
         : Component(entity)
-        , m_meshData(meshData)
+        , m_mesh(mesh)
         , m_shader(shader)
-        , m_vao(0)
-        , m_vbo(0)
-        , m_ibo(0)
     {
 
     }
 
-    Mesho::~Mesho()
+    void MeshComponent::render()
     {
-        destroy();
-    }
+        static auto shineDamper = 1.0f;
+        static auto reflectivity = 0.0f;
 
-    void Mesho::create()
-    {
-
-        glGenVertexArrays(1, &m_vao);
-        glBindVertexArray(m_vao);
-
-        // Bind vbo
-        glGenBuffers(1, &m_vbo);
-        // Array Buffer Size for Vertices = Number of Vertices * size of each vertex 
-        glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-        glBufferData(GL_ARRAY_BUFFER, m_meshData.m_vertices.size() * sizeof(VertexInfo), &m_meshData.m_vertices[0], GL_STATIC_DRAW);
-
-        // Bind ibo
-        glGenBuffers(1, &m_ibo);
-        // Array Buffer Size for Indices = Number of Indices * size of each index (unsigned int)
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_meshData.m_indices.size() * sizeof(unsigned int), &m_meshData.m_indices[0], GL_STATIC_DRAW);
-
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexInfo), (void*)(offsetof(VertexInfo, VertexInfo::m_position)));
-
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(VertexInfo), (void*)(offsetof(VertexInfo, VertexInfo::m_normal)));
-
-        glEnableVertexAttribArray(2);
-        glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(VertexInfo), (void*)(offsetof(VertexInfo, VertexInfo::m_color)));
-
-        glBindVertexArray(0);
-
-    }
-
-    void Mesho::setTexture(const std::string & textureName, GLuint textureId)
-    {
-        if (textureId != 0)
-        {
-            m_textures[textureName] = textureId;
-        }
-        else
-        {
-            fprintf(stderr, "Failed to add new texture with name %s", textureName.c_str());
-        }
-    }
-
-    GLuint Mesho::getTextureId(const std::string& texture) const
-    {
-        return m_textures.at(texture);
-    }
-
-    GLuint Mesho::getVao() const
-    {
-        return m_vao;
-    }
-
-    GLuint Mesho::getVbo() const
-    {
-        return m_vbo;
-    }
-
-    GLuint Mesho::getIbo() const
-    {
-        return m_ibo;
-    }
-
-    Shader& Mesho::getShader() const
-    {
-        return m_shader;
-    }
-
-    void Mesho::destroy()
-    {
-        glDeleteVertexArrays(1, &m_vao);
-        glDeleteBuffers(1, &m_vbo);
-        glDeleteBuffers(1, &m_ibo);
-    }
-
-    MeshData& Mesho::getMeshData()
-    {
-        return m_meshData;
-    }
-
-    void Mesho::update()
-    {
-
-    }
-
-    void Mesho::render()
-    {
-
-        static Material m_material = { {1.f, 1.f, 1.f}, {0.1f, 0.1f, 0.1f}, {1.f, 1.f, 1.f}, {10.f} };
-
-        ImGui::Begin("Material");
-        ImGui::SliderFloat3("ambient", &m_material.ambient.x, 0.f, 1.f);
-        ImGui::SliderFloat3("diffuse", &m_material.diffuse.x, 0.f, 1.f);
-        ImGui::SliderFloat3("specular", &m_material.specular.x, 0.f, 1.f);
-        ImGui::SliderFloat("shininess", &m_material.shininess, 0.f, 100.f);
-        ImGui::End();
-
-        m_shader.use();
-
-        m_shader.setVec3("material.ambient", m_material.ambient);
-        m_shader.setVec3("material.diffuse", m_material.diffuse);
-        m_shader.setVec3("material.specular", m_material.specular);
-        m_shader.setFloat("material.shininess", m_material.shininess);
-        m_shader.setMat4("model", getEntity()->getTransform().getModelMatrix());
-        glBindVertexArray(m_vao);
-        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(m_meshData.m_indices.size()), GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
+        auto modelMatrix = getEntity()->getTransform().getModelMatrix();
+        m_shader->use();
+        m_shader->setMat4("model", modelMatrix);
+        m_shader->setFloat("shineDamper", shineDamper);
+        m_shader->setFloat("reflectivity", reflectivity);
+        m_shader->unuse();
+        m_mesh->render(*m_shader);
     }
 
 }
--- src/systems/entity_component_system/components/mesh_component.hpp
+++ src/systems/entity_component_system/components/mesh_component.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
-#include "systems/render_system/vertex_info.hpp"
-#include "systems/shader_system/shader.hpp"
-#include "systems/render_system/material.hpp"
 #include "systems/entity_component_system/components/component.hpp"
-#include "systems/render_system/mesh_data.hpp"
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
@@ -17,37 +13,22 @@ namespace aiko
 {
 
     class Entity;
+    class Mesh;
+    class Shader;
 
-    class Mesho : public Component
+    class MeshComponent : public Component
     {
     public:
 
-        Mesho(Entity* entity, Shader& shader, MeshData& meshData);
-        ~Mesho();
+        MeshComponent(Entity* entity, Mesh* mesh, Shader* shader);
+        virtual ~MeshComponent() = default;
 
-        void create();
-
-        void setTexture(const std::string& textureName, GLuint textureId);
-        GLuint getTextureId(const std::string& texture) const;
-        GLuint getVao() const;
-        GLuint getVbo() const;
-        GLuint getIbo() const;
-        Shader& getShader() const;
-
-        void update();
-        void render();
-        void destroy();
-
-        MeshData& getMeshData();
+        virtual void render() override;
 
     private:
 
-        MeshData m_meshData;
-        Shader& m_shader;
-        GLuint m_vao;
-        GLuint m_vbo;
-        GLuint m_ibo;
-        std::map<std::string, GLuint> m_textures;
+        Mesh*   m_mesh;
+        Shader* m_shader;
 
     };
 }
