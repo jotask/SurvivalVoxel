540bf4b4e5a404983b3efb80ecd1be3a109942d6
Jose Vives Iznardo <acachitoro@gmail.com>
2020-06-10 21:02:41 +0200

    Adding a duplicated texture2d for testing. Later state will be merged into current texture definition

---
+++ src/systems/assets_system/texture2D.cpp
@@ -0,0 +1,52 @@
+#include "texture2D.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace aiko
+{
+    Texture2D::Texture2D()
+        : m_id(-1)
+        , m_width(0)
+        , m_height(0)
+        , m_internalFormat(GL_RGB)
+        , m_imageFormat(GL_RGB)
+        , m_wrap_S(GL_REPEAT)
+        , m_wrap_T(GL_REPEAT)
+        , m_filterMin(GL_LINEAR)
+        , m_filterMax(GL_LINEAR)
+    {
+
+    }
+
+    GLuint Texture2D::getId() const
+    {
+        return m_id;
+    }
+
+    void Texture2D::generate(GLuint width, GLuint height, unsigned char * data)
+    {
+
+        // FIXME :: Dirty right now but it works
+        /// we don't want to generate more textures, only one for texture that is modiflied
+        if (m_id == -1)
+        {
+            glGenTextures(1, &m_id);
+        }
+
+        m_width = width;
+        m_height = height;
+        glBindTexture(GL_TEXTURE_2D, m_id);
+        glTexImage2D(GL_TEXTURE_2D, 0, m_internalFormat, m_width, m_height, 0, m_imageFormat, GL_UNSIGNED_BYTE, data);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, m_wrap_S);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, m_wrap_T);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, m_filterMin);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, m_filterMax);
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    void Texture2D::bind()
+    {
+        glBindTexture(GL_TEXTURE_2D, m_id);
+    }
+
+}
